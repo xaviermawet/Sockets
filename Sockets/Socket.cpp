ef9c8df9f4c4f5a10340cac629c01d30934f95da
Xavier Mawet <Nakim@NakimMac.local>
2014-11-08 17:54:24 +0100

    Added new files for Socket class

---
+++ Sockets/Socket.cpp
@@ -0,0 +1,9 @@
+//
+//  Socket.cpp
+//  Sockets
+//
+//  Created by Xavier Mawet on 8/11/14.
+//  Copyright (c) 2014 Xavier Mawet. All rights reserved.
+//
+
+#include "Socket.h"
---
+++ Sockets/Socket.h
@@ -0,0 +1,14 @@
+//
+//  Socket.h
+//  Sockets
+//
+//  Created by Xavier Mawet on 8/11/14.
+//  Copyright (c) 2014 Xavier Mawet. All rights reserved.
+//
+
+#ifndef __Sockets__Socket__
+#define __Sockets__Socket__
+
+#include <iostream>
+
+#endif /* defined(__Sockets__Socket__) */
