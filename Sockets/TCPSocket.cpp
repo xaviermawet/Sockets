50c34da7b3bf2102e228b9b326bf6598a0643819
Xavier Mawet <Nakim@NakimMac.local>
2014-11-09 00:07:56 +0100

    Added TCPSocket class (Socket subclass)

---
+++ Sockets/TCPSocket.cpp
@@ -0,0 +1,28 @@
+//
+//  TCPSocket.cpp
+//  Sockets
+//
+//  Created by Xavier Mawet on 8/11/14.
+//  Copyright (c) 2014 Xavier Mawet. All rights reserved.
+//
+
+#include "TCPSocket.h"
+
+TCPSocket::TCPSocket(void)
+    : Socket(socket_type::TCP)
+{
+    // Nothing to do here ...
+}
+
+TCPSocket::TCPSocket(const TCPSocket& copy_tcpsocket)
+    : Socket(copy_tcpsocket)
+{
+    
+}
+
+TCPSocket& TCPSocket::operator=(const TCPSocket &copy_tcpsocket)
+{
+    Socket::operator=(copy_tcpsocket);
+    
+    return *this;
+}
---
+++ Sockets/TCPSocket.h
@@ -0,0 +1,25 @@
+//
+//  TCPSocket.h
+//  Sockets
+//
+//  Created by Xavier Mawet on 8/11/14.
+//  Copyright (c) 2014 Xavier Mawet. All rights reserved.
+//
+
+#ifndef __Sockets__TCPSocket__
+#define __Sockets__TCPSocket__
+
+#include "Socket.h"
+
+class TCPSocket : public Socket
+{
+    public:
+    
+        explicit TCPSocket(void);
+        explicit TCPSocket(const TCPSocket& copy_tcpsocket);
+        TCPSocket& operator=(const TCPSocket& copy_tcpsocket);
+    
+    protected:
+};
+
+#endif /* defined(__Sockets__TCPSocket__) */
